ff054c2742ce8fcb1113cebd10ac9a1a0791678d
Maxim Bodrov <activer12@mail.ru>
2022-08-24 17:26:32 +0300

    Добавил новые проверки

--- code/check.py
+++ code/check.py
@@ -1,19 +1,48 @@
 from os.path import exists
-from os import mkdir, getcwd
+from os import mkdir, getcwd, listdir
+from settings import url_set_file, app_files
+from requests import get
 
 
 def main_check():
     app_dir = getcwd()
+
+    empty, files = check_empty_folder(app_dir)
+    if not empty:
+        print(f'App directory not empty!\nPlease delete or remove files:')
+        for i in files:
+            print(f'\t{i}')
     check_folder(app_dir)
     check_settings()
 
 
+def check_empty_folder(check_dir, exept_files=app_files):
+    all_files = listdir(check_dir)
+    all_files = list(filter(lambda x: x not in exept_files, all_files))
+    if len(all_files) > 0:
+        return False, all_files
+    return True
+
+
 def check_folder(wd: str):
-    if not exists('data'):
+    if not exists(f'{wd}/data'):
         mkdir('data')
 
 
 def check_settings():
     if not exists('data/settings.ini'):
         with open('data/settings.ini', 'w') as set_file:
-            pass
+            set_file.write(get(f'{url_set_file}?raw=true').text)
+
+
+def download_file(url):
+    local_filename = url.split('/')[-1]
+    with get(url, stream=True, allow_redirects=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    return local_filename
+
+
+main_check()
---
+++ code/settings.py
@@ -0,0 +1,16 @@
+
+# ! URLS
+url_set_file = 'https://github.com/MaxBro12/mylauncher/blob/master/source/settings.ini'
+
+
+# ! Files
+app_files = [
+    '__pycache__',
+    'data',
+    'check.py',
+    'mylaunch.py',
+    'requirements.txt',
+    'settings.py',
+    'userinp.py',
+    'mylaunch.exe',
+]
